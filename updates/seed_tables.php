4c8d0327cbfbe13524b781837ba4dfd30c284ff6
Rob Ballantyne <rob@dynamedia.uk>
2021-07-02 11:55:22 +0100

    Remove CMS partial capability for now, it is unreliable due to page cycle. Will fix

--- updates/seed_tables.php
+++ updates/seed_tables.php
@@ -68,7 +68,7 @@ class SeedAllTables extends Seeder
                 'is_published' => true,
                 'author_id' => User::first()->id,
                 'editor_id' => User::first()->id,
-                'show_contents' => true,
+                'show_contents' => false,
                 'primary_category_id' => Category::first()->id,
                 'body' => [
                     [
@@ -76,7 +76,10 @@ class SeedAllTables extends Seeder
                             'sId' => 'first',
                             'in_contents' => true,
                             'heading' => 'A content block',
-                            'content' => 'Construct your posts using as many or as few blocks as you like!',
+                            'content' => '
+                                <p>Construct your posts using as many or as few blocks as you like!</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            ',
                         ],
                         '_group' => 'section'
                     ],
@@ -85,12 +88,35 @@ class SeedAllTables extends Seeder
                             'sId' => 'second',
                             'in_contents' => true,
                             'heading' => 'Another content block',
-                            'content' => 'Construct your posts using as many or as few blocks as you like!',
+                            'content' => '
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                            ',
                         ],
                         '_group' => 'section'
                     ]
                 ],
             ]);
+            if ($post->slug == 'first-blog-post') {
+                $extendBody = [
+                    [
+                        '_group' => 'pagebreak',
+                    ],
+                    [
+                        'block' => [
+                            'sId' => 'third',
+                            'in_contents' => true,
+                            'heading' => 'New page content',
+                            'content' => '
+                                <p>Posts can, if you want, be written over multiple pages. It\'s all handled internally so just add a pagebreak block. Easy!</p>
+                            ',
+                        ],
+                        '_group' => 'section'
+                    ]
+                ];
+                $post->show_contents = true;
+                $post->body = array_merge($post->body, $extendBody);
+                $post->save();
+            }
             $post->tags()->attach(Tag::first()->id);
         }
     }

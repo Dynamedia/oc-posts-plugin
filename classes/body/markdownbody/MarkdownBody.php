9248306e0242fd1affe641f935ead0d7b4ce736d
Rob Ballantyne <rob@dynamedia.uk>
2021-09-02 15:26:39 +0100

    Actuallly parse the markdown

--- classes/body/markdownbody/MarkdownBody.php
+++ classes/body/markdownbody/MarkdownBody.php
@@ -1,6 +1,7 @@
 <?php namespace Dynamedia\Posts\Classes\Body\Markdownbody;
 
 use Dynamedia\Posts\Classes\Body\Body;
+use Markdown;
 
 class MarkdownBody extends Body
 {
@@ -8,7 +9,7 @@ class MarkdownBody extends Body
     public function __construct($bodyDocument)
     {
         // For now, just explode on a horizontal rule with pagebreak class
-        $this->pages = explode('<hr class="pagebreak">', $bodyDocument['markdown_body']);
+        $this->pages = explode('<hr class="pagebreak">', Markdown::parse($bodyDocument['markdown_body']));
     }
 
 }
